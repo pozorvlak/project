b1016f3adb55d9cd3ed67ed793717dd37a08777a
Miles Gould <miles@assyrian.org.uk>
2012-12-24 17:22:08 +0000

    Copy in pre-existing .rvm

--- script/setup_growstuff.sh
+++ script/setup_growstuff.sh
@@ -6,16 +6,22 @@ if [ $# -lt 1 ]; then
   exit 2;
 fi
 
-curl -L https://get.rvm.io | bash -s stable
-. ~/.rvm/scripts/rvm # uses bashisms
-rvm install 1.9.3-p194
+cd
+if [ "$HOSTNAME" = "trowel.growstuff.org" ]; then
+  echo "You're on the hack server! Copying pre-existing ~/.rvm"
+  tar xzf /usr/local/share/rvmdir.tar.gz
+  . ~/.rvm/scripts/rvm # uses bashisms
+else
+  curl -L https://get.rvm.io | bash -s stable
+  . ~/.rvm/scripts/rvm # uses bashisms
+  rvm install 1.9.3-p194
+  rvm docs generate # not sure we actually need this -MRG
+fi
 rvm use 1.9.3-p194
 rvm --default 1.9.3-p194
-# rvm docs generate # not sure we actually need this -MRG
 git clone https://github.com/$1/growstuff.git
 cd growstuff/
 git remote add upstream https://github.com/Growstuff/growstuff.git
-rvm use 1.9.3-p194
 rvm gemset create growstuffdev
 rvm gemset use growstuffdev
 gem install bundler
